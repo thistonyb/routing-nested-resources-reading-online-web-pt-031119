bd44d22b0633ebe555e6eb5988a7b574dfd9053f
Tony Baker <this.tonybaker@gmail.com>
2019-08-12 18:31:02 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     #nested resource for posts
     resources :posts, only: [:show, :index]
   end
-  
+
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
